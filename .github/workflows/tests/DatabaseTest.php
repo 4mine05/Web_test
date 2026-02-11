c7f0c44955f494cfa7d035fdf00aa72965e70b68
Mohamed amine Lachhab <mohamed.amine@alumno.iesluisvives.org>
2026-02-11 16:34:06 +0100

    Create DatabaseTest.php

---
+++ .github/workflows/tests/DatabaseTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class DatabaseTest extends TestCase
+{
+    private $conexion;
+
+    protected function setUp(): void
+    {
+        $this->conexion = mysqli_connect(
+            getenv('DB_HOST') ?: '127.0.0.1',
+            'root',
+            'foro_pass',
+            'foro',
+            3306
+        );
+    }
+
+    public function testConexionBD()
+    {
+        $this->assertNotFalse($this->conexion);
+    }
+
+    public function testSelectBasico()
+    {
+        $result = mysqli_query($this->conexion, "SELECT 1");
+        $this->assertNotFalse($result);
+    }
+}
