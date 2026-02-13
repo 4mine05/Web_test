d98aaf8cf76d61c08c7353f71910a7d187f30abf
Mohamed amine Lachhab <mohamed.amine@alumno.iesluisvives.org>
2026-02-13 19:12:13 +0100

    Update bbdd.php

--- BBDD/config/bbdd.php
+++ BBDD/config/bbdd.php
@@ -3,7 +3,7 @@
 $DB_HOST = 'db';
 $DB_USER = 'root';
 $DB_PASS = 'foro_pass';
-$DB_NAME = 'foro_social';
+$DB_NAME = 'foro';
 $DB_PORT = 3306;
 
 $conexion = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, $DB_PORT);
