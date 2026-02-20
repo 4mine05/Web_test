1d1b870dae526fb7f1ebb4912ac316dcd37c9849
Mohamed amine Lachhab <mohamed.amine@alumno.iesluisvives.org>
2026-02-20 18:50:22 +0100

    Use environment variables for database configuration

--- BBDD/config/bbdd.php
+++ BBDD/config/bbdd.php
@@ -1,9 +1,9 @@
 <?php
 
-$DB_HOST = 'db';
-$DB_USER = 'root';
-$DB_PASS = 'foro_pass';
-$DB_NAME = 'foro';
+$DB_HOST = getenv('DB_HOST') ?: 'db';
+$DB_USER = getenv('DB_USER') ?: 'root';
+$DB_PASS = getenv('DB_PASS') ?: 'foro_pass';
+$DB_NAME = getenv('DB_NAME') ?: 'foro';
 $DB_PORT = 3306;
 
 $conexion = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, $DB_PORT);
